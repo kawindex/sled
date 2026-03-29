88eae1f7554a728e3fb8309d624d2586f7da7af9
Kawin <kawin.open@gmail.com>
2026-03-29 00:26:11 -0400

    python/test: add round trip test for long `hex` using Lorem ipsum

--- python/tests/test_hex/test_hex_lorem.py
+++ python/tests/test_hex/test_hex_lorem.py
@@ -6,7 +6,21 @@ import pytest
 import pysled
 
 
-class TestHexLorem:
+@pytest.fixture(scope="module")
+def lorem_ipsum() -> bytes:
+    return (
+        b'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do '
+        b'eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        b'Ut enim ad minim veniam, quis nostrud exercitation ullamco '
+        b'laboris nisi ut aliquip ex ea commodo consequat. Duis aute '
+        b'irure dolor in reprehenderit in voluptate velit esse cillum '
+        b'dolore eu fugiat nulla pariatur. Excepteur sint occaecat '
+        b'cupidatat non proident, sunt in culpa qui officia deserunt '
+        b'mollit anim id est laborum.'
+    )
+
+
+class TestHexLoremParse:
     @pytest.fixture(scope="class")
     def sled_path(self, core_test_data_dir: Path) -> Path:
         return core_test_data_dir.joinpath("hex-lorem.sd")
@@ -14,19 +28,6 @@ class TestHexLorem:
     @pytest.fixture(scope="class")
     def sled_text(self, sled_path: Path) -> str:
         return sled_path.read_text().strip()
-
-    @pytest.fixture(scope="class")
-    def lorem_ipsum(self) -> bytes:
-        return (
-            b'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do '
-            b'eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-            b'Ut enim ad minim veniam, quis nostrud exercitation ullamco '
-            b'laboris nisi ut aliquip ex ea commodo consequat. Duis aute '
-            b'irure dolor in reprehenderit in voluptate velit esse cillum '
-            b'dolore eu fugiat nulla pariatur. Excepteur sint occaecat '
-            b'cupidatat non proident, sunt in culpa qui officia deserunt '
-            b'mollit anim id est laborum.'
-        )
 
     @pytest.fixture(scope="class")
     def expected_data(self, lorem_ipsum: bytes) -> Dict[str, pysled.Entity]:
@@ -47,3 +48,31 @@ class TestHexLorem:
     ) -> None:
         actual_data = pysled.from_sled(sled_text)
         assert expected_data == actual_data
+
+
+class TestHexLoremRoundTrip:
+    @pytest.fixture(scope="class")
+    def arbitrary_key(self) -> str:
+        return "bin"
+
+    @pytest.fixture(scope="class")
+    def expected_data(
+        self, lorem_ipsum: bytes, arbitrary_key: str
+    ) -> Dict[str, bytes]:
+        return {arbitrary_key: lorem_ipsum}
+
+    def test_round_trip(self, expected_data: Dict[str, bytes]) -> None:
+        sled_text = pysled.to_sled(expected_data)
+        round_trip_data = pysled.from_sled(sled_text)
+        assert expected_data == round_trip_data
+
+    def test_round_trip_mini(self, expected_data: Dict[str, bytes]) -> None:
+        sled_text = pysled.to_sled_mini(expected_data)
+        round_trip_data = pysled.from_sled(sled_text)
+        assert expected_data == round_trip_data
+
+    def test_round_trip_basic(self, expected_data: Dict[str, bytes]) -> None:
+        serializer = pysled._serializer_basic.SledSerializerBasic()
+        sled_text = serializer.to_sled(expected_data)
+        round_trip_data = pysled.from_sled(sled_text)
+        assert expected_data == round_trip_data
