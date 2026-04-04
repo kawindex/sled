7a200d90959042e32dbca38c7830b98070040403
Kawin <kawin.open@gmail.com>
2026-04-04 11:01:21 -0400

    python/test: add tests for invalid serializer kwargs

---
+++ python/tests/test_serializer_kwargs_invalid.py
@@ -0,0 +1,78 @@
+from typing import Any, Dict, NamedTuple
+import pytest
+
+import pysled
+
+
+@pytest.fixture(scope="module", params=("-", ">", " | ", "\t\\"))
+def invalid_ser_arg_indent(request: pytest.FixtureRequest) -> str:
+    return request.param
+
+
+class HexLength(NamedTuple):
+    hex_bytes_per_separator: int
+    hex_line_length: int
+
+
+@pytest.fixture(
+    scope="module",
+    params=(
+        HexLength(2, -20),
+        HexLength(-2, -20),
+        HexLength(5, 8),
+        HexLength(-5, 8),
+        HexLength(8, 10),
+        HexLength(-8, 10),
+    ),
+)
+def invalid_ser_args_hex_length(request: pytest.FixtureRequest) -> HexLength:
+    return request.param
+
+
+class TestToSledKwargsInvalid:
+    @pytest.fixture(scope="class")
+    def arbitrary_data(self) -> Dict[str, pysled.Entity]:
+        return {"greeting": "Hello, World!"}
+
+    def test_invalid_indent(
+        self,
+        invalid_ser_arg_indent: str,
+        arbitrary_data: Dict[str, pysled.Entity],
+    ) -> None:
+        with pytest.raises(ValueError):
+            pysled.to_sled(arbitrary_data, indent=invalid_ser_arg_indent)
+
+    def test_invalid_hex_length(
+        self,
+        invalid_ser_args_hex_length: HexLength,
+        arbitrary_data: Dict[str, pysled.Entity],
+    ) -> None:
+        with pytest.raises(ValueError):
+            pysled.to_sled(
+                arbitrary_data,
+                hex_bytes_per_separator=invalid_ser_args_hex_length.hex_bytes_per_separator,
+                hex_line_length=invalid_ser_args_hex_length.hex_line_length,
+            )
+
+
+class TestSerializerKwargsInvalid:
+    def test_invalid_indent(self, invalid_ser_arg_indent: str) -> None:
+        with pytest.raises(ValueError):
+            pysled.SledSerializer(indent=invalid_ser_arg_indent)
+
+    def test_invalid_hex_length(
+        self, invalid_ser_args_hex_length: HexLength
+    ) -> None:
+        with pytest.raises(ValueError):
+            pysled.SledSerializer(
+                hex_bytes_per_separator=invalid_ser_args_hex_length.hex_bytes_per_separator,
+                hex_line_length=invalid_ser_args_hex_length.hex_line_length,
+            )
+
+
+class TestSerializerBasicKwargsInvalid:
+    def test_invalid_indent(self, invalid_ser_arg_indent: str) -> None:
+        with pytest.raises(ValueError):
+            pysled._serializer_basic.SledSerializerBasic(
+                indent=invalid_ser_arg_indent
+            )
