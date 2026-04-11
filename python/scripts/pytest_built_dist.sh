49d6ab75eee2d45d12257aa38b7bb52e6870ecee
Kawin <kawin.open@gmail.com>
2026-04-11 14:24:33 -0400

    ci/python: add build and publish workflow

---
+++ python/scripts/pytest_built_dist.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# Set any pipeline to return the last non-zero status
+# so that we can capture an error in uv run pytest
+# even after piping its stdout into tee
+set -o pipefail
+
+# Run in repo's python/ directory
+cd ${GITHUB_WORKSPACE}/python/
+
+# --cov-report=markdown-append only appends the coverage report.
+# When testing the distribution, we are more concerned with
+# test results than coverage, so across all Python versions,
+# we take the terminal output and manually append it
+# to $GITHUB_STEP_SUMMARY
+
+echo '```' >> ${GITHUB_STEP_SUMMARY}
+
+uv run \
+  --isolated \
+  --no-project \
+  --no-cache \
+  --with ${parsled_DIST_PATH} \
+  --with pytest \
+  --with pytest-cov \
+  pytest \
+    --cov=parsled \
+    --cov-report=term-missing \
+    ${GITHUB_WORKSPACE}/python/tests/ \
+| tee --append ${GITHUB_STEP_SUMMARY}
+
+TEST_EXIT_CODE=${?}
+
+echo '```' >> ${GITHUB_STEP_SUMMARY}
+
+exit ${TEST_EXIT_CODE}
